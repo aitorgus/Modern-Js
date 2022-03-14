82937fa4604d9328f4cebfc7d3d4c0816da7160c
Aitor <aitor_gustavo@hotmail.com>
2022-03-14 21:33:09 +0100

    Terminada Lecciones 1 y 2

--- PracticandoJavascript/Leccion02/2-04-TiposDatosJavascript.js
+++ PracticandoJavascript/Leccion02/2-04-TiposDatosJavascript.js
@@ -1,1 +1,10 @@
 //Buenas prácticas para nombres de Variables
+//Utilizamos la metodologia cammell para nombrar a las variables.
+let nombreCompleto;
+nombreCompleto = "Juan Perez";
+console.log(nombreCompleto);
+
+let x, y;
+x = 10, y = 20;
+let z = x + y; 
+console.log(z);
---
+++ PracticandoJavascript/Leccion02/2-05-TiposDatosJavascript.js
@@ -0,0 +1,9 @@
+let nombreCompleto = "Juan Perez";
+let nombrecomleto = "Carlos Lara";
+//Js es sensible a minusculas y mayúsculas
+console.log(nombreCompleto);
+console.log(nombrecomleto);
+
+//El nombre de una variable no puede empezar por un número.
+
+//No podemos usar las palabras reservadas como variable.
